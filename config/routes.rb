3531c48ea18149c755f6cff451e5c9a7e0c6a42c
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-30 03:30:56 +0000

    starting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+# index as action name is used for displaying lists 
+  get("/directors", { :controller => "directors", :action =."index"})
 end
