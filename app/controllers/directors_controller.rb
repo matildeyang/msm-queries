aca74e73aec3fa349069632b3613fb218fd0a1e6
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-30 20:31:23 +0000

    director links

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,13 @@ class DirectorsController < ApplicationController
   def index 
     render({ :template => "director_templates/list"})
   end 
+
+  def show 
+   the_id = params.fetch("the_id")
+
+   # use this to retrieve existing data 
+   matching_records  = Director.where({ :id => the_id })
+   @the_director = matching_records.at(0)
+   render({ :template => "director_templates/details"})
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 # index as action name is used for displaying lists 
   get("/directors", { :controller => "directors", :action => "index"})
+
+  get("/directors/:the_id", { :controller => "directors", :action => "show"})
 end
