766a0b3e21e3e19db7e04e12360111ccb5009b2b
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-31 21:07:03 +0000

    code for oldest and youngest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -15,12 +15,32 @@ class DirectorsController < ApplicationController
   def youngest 
     birthday_directors = Director.where.not({ :dob => nil })
     age = 0 
+    youngest_director = nil 
     birthday_directors.all.each do |a_director|
       director_age = Date.today - a_director.dob
-      
+      if age > director_age
+        age = age 
+        youngest_director = youngest_director
+      else 
+        age = director_age
+        youngest_director = a_director.name
+      end 
     end 
   end 
 
   def oldest 
+    birthday_directors = Director.where.not({ :dob => nil })
+    age = 0 
+    oldest_director = nil 
+    birthday_directors.all.each do |a_director|
+      director_age = Date.today - a_director.dob
+      if age > director_age
+        age = age 
+        oldest_director = oldest_director
+      else 
+        age = director_age
+        oldest_director = a_director.name
+      end 
+    end 
   end 
 end 
