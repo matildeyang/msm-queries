ce1fd1a66f61c3d8aa6964b1a61bcfda1fe127a5
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-31 21:26:13 +0000

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index"})
   get("/directors/:the_id", { :controller => "directors", :action => "show"})
   get("/directors/youngest", { :controller => "directors", :action => "youngest"})
-  get("/directors/youngest", { :controller => "directors", :action => "oldest"})
+  get("/directors/eldest", { :controller => "directors", :action => "oldest"})
 
   get("/actors", { :controller => "actors", :action => "index"})
   get("/actors/:the_id", { :controller => "actors", :action => "show"})
